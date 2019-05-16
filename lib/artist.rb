076cd1a26edf2488d4b650e8f14cd92d764d286c
Christian Pinto <pintochristian2@gmail.com>
2019-05-16 15:13:21 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+
+  attr_accessor :name ,:genre, :song
+
+  # def intialize(name, genre)
+  #   @name = name
+  #   @genre = genre
+  # end
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  # def intialize(name)
+  #   @title = name
+  # end
+
+end
